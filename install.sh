b1e3dc81eea54950224787f1efda8694b2ea87e1
np <nerpoc42@gmail.com>
2024-01-03 11:48:05 +0000

    updated install script

--- install.sh
+++ install.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+# Setup virtual python environment
+if [ ! -d venv/ ]; then
+    python -m venv venv
+fi
+
+# Activate virtual python environment
+source venv/bin/activate
+
+# Install required packages
+pip install -r requirements.txt
+
+if [! (dpkg -s tk)]; then
+    sudo apt-get install tk
+fi
+
+# Install desktop icon
 cp install/saldainiai.desktop ~/Desktop/
