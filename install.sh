81d1ed7b6922a3baf931f58d00e0402685f25fa8
np <nerpoc42@gmail.com>
2024-01-03 12:11:28 +0000

    updated install script and desktop

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-# Setup virtual python environment
+
+root_dir=$(dirname "$0")
+
+echo "Changing directory to project root"
+cd $(echo $root_dir)
+
+echo "Setting up virtual environment"
 if [ ! -d venv/ ]; then
     python -m venv venv
 fi
 
-# Activate virtual python environment
+echo "Activating virtual environment"
 source venv/bin/activate
 
-# Install required packages
+echo "Installing required libraries"
 pip install -r requirements.txt
 
 dpkg -s tk > /dev/null
@@ -15,5 +21,8 @@ if [ $? -ne 0 ]; then
     sudo apt-get install tk
 fi
 
-# Install desktop icon
+echo "Installing executable"
+sudo ln run.sh /usr/local/bin/saldainiai
+
+echo "Installing desktop icon"
 cp install/saldainiai.desktop ~/Desktop/
