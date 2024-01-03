c722fd0a5245811266b68f19bca16fd0d008c026
np <nerpoc42@gmail.com>
2024-01-03 10:58:47 +0000

    modified input test

--- tests/input_test.py
+++ tests/input_test.py
@@ -3,14 +3,25 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 
+out_pin = 31
 in_pin = 33
+
+GPIO.setup(out_pin, GPIO.OUT)
 GPIO.setup(in_pin, GPIO.IN)
 
 DELAY = 2
+val = True
+
 try:
     while True:
+        if val:
+            GPIO.output(out_pin, GPIO.HIGH)
+        else:
+            GPIO.output(out_pin, GPIO.LOW)
+
         res = GPIO.input(in_pin)
-        print(res)
+        print(f"Expected: {val}, Received: {res}")
+        val = not val
         time.sleep(DELAY)
 except KeyboardInterrupt:
     pass
