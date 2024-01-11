3ccceea57699c83cefe3914a2e42104dde3fa039
np <nerpoc42@gmail.com>
2024-01-11 12:06:15 +0000

    Refactored gui

--- src/gui.py
+++ src/gui.py
@@ -1,122 +1,141 @@
 import tkinter as tk
-import time
-from tkinter import messagebox 
+from tkinter import messagebox
 from PIL import Image, ImageTk
 from candy import ANANASAS, FORTUNA, request_candy
 
-countdown_step = 1500
+COUNTDOWN_STEP = 1500
+WINDOW_WIDTH = 1024
+WINDOW_HEIGHT = 600
 
 def on_exit():
     result = messagebox.askquestion("Exit", "Ar tikrai norite išeiti?")
     if result == "yes":
-        root.destroy()
+        window.destroy()
 
-root = tk.Tk()
-root.title("Saldainiai")
+def clear_frame(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
 
-root.attributes('-fullscreen', True)
-root.attributes("-type", "splash")
+def view_catch_candy(candy):
+    clear_frame(frame_content)
 
-fortuna_img = Image.open("src/img/fortuna.png")
-fortuna_img = fortuna_img.resize((250, 200))
-fortuna_img = ImageTk.PhotoImage(fortuna_img)
-
-ananasas_img = Image.open("src/img/ananasas.png")
-ananasas_img = ananasas_img.resize((250, 250))
-ananasas_img = ImageTk.PhotoImage(ananasas_img)
-
-ku_img = Image.open("src/img/ku.png")
-ku_img = ku_img.resize((150, 87))
-ku_img = ImageTk.PhotoImage(ku_img)
-
-conexus_img = Image.open("src/img/conexus.png")
-conexus_img = conexus_img.resize((200, 100))
-conexus_img = ImageTk.PhotoImage(conexus_img)
-
-fondas_img = Image.open("src/img/fondas.png")
-fondas_img = fondas_img.resize((100, 35))
-fondas_img = ImageTk.PhotoImage(fondas_img)
-
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-
-window_width = 1024
-window_height = 600
-x_position = (screen_width - window_width) // 2
-y_position = (screen_height - window_height) // 2
-
-root.geometry(f"{window_width}x{window_height}+{x_position}+{y_position}")
-
-logos = tk.Frame(root)
-logos.pack(expand=True)
-
-conexus_logo = tk.Label(logos, width=300, height=100, image=conexus_img, borderwidth=0, relief="solid")
-conexus_logo.pack(side=tk.LEFT, padx=10)
-
-ku_logo = tk.Label(logos, width=300, height=60, image=ku_img, borderwidth=0, relief="solid")
-ku_logo.pack(side=tk.LEFT, padx=10)
-
-fondas_logo = tk.Label(logos, width=200, height=100, image=fondas_img, borderwidth=0, relief="solid")
-fondas_logo.pack(side=tk.LEFT, padx=10)
-
-frame = tk.Frame(root)
-frame.pack(expand=True)
-
-def clear_frame():
-   for widgets in frame.winfo_children():
-      widgets.destroy()
-
-#def pick_candy(candy):
-#    clear_frame()
-#
-#    label = tk.Label(frame, text="Saldainis imamas...", font=("Rando", 25))
-#    label.pack()
-    
-#    request_candy(candy, lambda: catch_candy(candy))
-      
-def pick_candy(candy):
-    clear_frame()
-
-    label = tk.Label(frame, text="Gaudykite saldainį!", font=("Rando", 25))
+    label = tk.Label(frame_content, text="Gaudykite saldainį!", font=("Rando", 25))
     label.pack(pady=20)
 
-    countdown = tk.Label(frame, font=("Rando", 25))
+    countdown = tk.Label(frame_content, font=("Rando", 25))
     countdown.pack()
-    received_signal = False
 
-    def return_home():
-        nonlocal received_signal
-        received_signal = True
-        show_main_interface()
+    # Remembers if robot has returned a signal, to prevent countdown
+    robot_responded = False
+
+    def on_robot_response():
+        nonlocal robot_responded
+        robot_responded = True
+
+        view_select_candy()
 
     def show_count(count):
-        nonlocal received_signal
-        if received_signal:
+        nonlocal robot_responded
+        if robot_responded:
+            # If robot already responded, do not continue counting
             return
 
         countdown.config(text=str(count))
 
-    request_candy(candy, return_home)
+    request_candy(candy, on_robot_response)
 
-    frame.after(1*countdown_step, lambda: show_count(3))
-    frame.after(2*countdown_step, lambda: show_count(2))
-    frame.after(3*countdown_step, lambda: show_count(1))
+    frame_content.after(1 * COUNTDOWN_STEP, lambda: show_count(3))
+    frame_content.after(2 * COUNTDOWN_STEP, lambda: show_count(2))
+    frame_content.after(3 * COUNTDOWN_STEP, lambda: show_count(1))
 
-# Create the main window
-def show_main_interface():
-    clear_frame()
+def view_select_candy():
+    clear_frame(frame_content)
 
-    question_label = tk.Label(frame, text="Kokio saldainio norite?", font=("Rando", 30))
+    question_label = tk.Label(frame_content, text="Kokio saldainio norite?", font=("Rando", 30))
     question_label.pack(pady=20)
 
-    fortune_button = tk.Button(frame, command=lambda: pick_candy(FORTUNA), width=300, height=200, image=fortuna_img, borderwidth=0, relief="solid")
+    fortune_button = tk.Button(frame_content, command=lambda: view_catch_candy(FORTUNA), width=300, height=200, image=fortuna_img, borderwidth=0, relief="solid")
     fortune_button.pack(side=tk.LEFT, padx=10)
 
-    ananasu_button = tk.Button(frame, command=lambda: pick_candy(ANANASAS), width=300, height=200, image=ananasas_img, borderwidth=0, relief="solid")
+    ananasu_button = tk.Button(frame_content, command=lambda: view_catch_candy(ANANASAS), width=300, height=200, image=ananasas_img, borderwidth=0, relief="solid")
     ananasu_button.pack(side=tk.RIGHT, padx=10)
-    
-    exit_button = tk.Button(root, text="X", font=("Rando", 20), command=on_exit)
+
+# Loads used images and set up their sizes
+def load_logos():
+    global ku_img, conexus_img, fondas_img, fortuna_img, ananasas_img
+
+    # University logos
+    ku_img = Image.open("src/img/ku.png")
+    ku_img = ku_img.resize((150, 87))
+    ku_img = ImageTk.PhotoImage(ku_img)
+
+    conexus_img = Image.open("src/img/conexus.png")
+    conexus_img = conexus_img.resize((200, 100))
+    conexus_img = ImageTk.PhotoImage(conexus_img)
+
+    fondas_img = Image.open("src/img/fondas.png")
+    fondas_img = fondas_img.resize((100, 35))
+    fondas_img = ImageTk.PhotoImage(fondas_img)
+
+    # Candy logos
+    fortuna_img = Image.open("src/img/fortuna.png")
+    fortuna_img = fortuna_img.resize((250, 200))
+    fortuna_img = ImageTk.PhotoImage(fortuna_img)
+
+    ananasas_img = Image.open("src/img/ananasas.png")
+    ananasas_img = ananasas_img.resize((250, 250))
+    ananasas_img = ImageTk.PhotoImage(ananasas_img)
+
+def main():
+    # Load logos used in the application
+    load_logos()
+
+    # Create main window in fullscreen
+    global window
+    window = tk.Tk()
+    window.title("Saldainiai")
+
+    window.attributes('-fullscreen', True)
+    window.attributes("-type", "splash")
+
+    # Setup screen resolution (TODO: Is this part required?)
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+
+    x_position = (screen_width - WINDOW_WIDTH) // 2
+    y_position = (screen_height - WINDOW_HEIGHT) // 2
+
+    window.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}+{x_position}+{y_position}")
+
+    # Setup header of the window
+    global frame_header
+    frame_header = tk.Frame(window)
+    frame_header.pack(expand=True)
+
+    # Display logos in the header
+    conexus_logo = tk.Label(frame_header, width=300, height=100, image=conexus_img, borderwidth=0, relief="solid")
+    conexus_logo.pack(side=tk.LEFT, padx=10)
+
+    ku_logo = tk.Label(frame_header, width=300, height=60, image=ku_img, borderwidth=0, relief="solid")
+    ku_logo.pack(side=tk.LEFT, padx=10)
+
+    fondas_logo = tk.Label(frame_header, width=200, height=100, image=fondas_img, borderwidth=0, relief="solid")
+    fondas_logo.pack(side=tk.LEFT, padx=10)
+
+    # Display close button
+    exit_button = tk.Button(window, text="X", font=("Rando", 20), command=on_exit)
     exit_button.place(relx=1.0, x=-10, y=10, anchor="ne")
 
-show_main_interface()
-root.mainloop()
+    # Setup main content of window
+    global frame_content
+    frame_content = tk.Frame(window)
+    frame_content.pack(expand=True)
+
+    # Display main interface
+    view_select_candy()
+
+    # Window loop
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
